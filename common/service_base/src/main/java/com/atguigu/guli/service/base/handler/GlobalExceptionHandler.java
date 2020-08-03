ef0b1514b623a84be7a52749b41b23c89ce94e5d
18628283196 <zl546505720@163.com>
2020-08-03 23:16:21 +0800

    统一异常处理

--- common/service_base/src/main/java/com/atguigu/guli/service/base/handler/GlobalExceptionHandler.java
+++ common/service_base/src/main/java/com/atguigu/guli/service/base/handler/GlobalExceptionHandler.java
@@ -2,6 +2,9 @@ package com.atguigu.guli.service.base.handler;
 
 import com.atguigu.guli.common.base.result.R;
 import com.atguigu.guli.common.base.result.ResultCodeEnum;
+import com.atguigu.guli.common.base.util.ExceptionUtils;
+import lombok.extern.log4j.Log4j;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.jdbc.BadSqlGrammarException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -9,26 +12,30 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @ControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     @ResponseBody
     public R error(Exception e){
-        e.printStackTrace();
+        //打印错误日志到控制台上,并且也记录到错误日志文件中
+        //log.error(e.getMessage());//打印的错误日志并不准确，最好打印跟踪栈的具体信息，更能直接找到错误所在
+        log.error(ExceptionUtils.getMessage(e));//将错误跟踪栈的日志输出到文件中
+        //e.printStackTrace();//这只能输出到控制台中
         return R.error();
     }
 
     @ExceptionHandler(BadSqlGrammarException.class)
     @ResponseBody
     public R error(BadSqlGrammarException e){
-        e.printStackTrace();
+        log.error(ExceptionUtils.getMessage(e));
         return R.setResult(ResultCodeEnum.BAD_SQL_GRAMMAR);
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
     @ResponseBody
     public R error(HttpMessageNotReadableException e){
-        e.printStackTrace();
+        log.error(ExceptionUtils.getMessage(e));
         return R.setResult(ResultCodeEnum.JSON_PARSE_ERROR);
     }
 }
